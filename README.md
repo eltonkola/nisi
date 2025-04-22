f546c6148a22f37e89ecf39d8e6599d82eb5f82c
Elton Kola <eltonkola@gmail.com>
2025-04-22 18:11:05 -0400

    empty launcher app

--- README.md
+++
@@ -1,2 +0,0 @@
-# nisi
-short web playground
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,24 @@
-rootProject.name = "com.ekola.nisi"
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Nisi"
+include(":app")
+ 
