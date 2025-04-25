74e61c95e3c6b70e31f9cdaf21bba07cca803f38
Elton Kola <eltonkola@gmail.com>
2025-04-25 14:24:32 -0400

    background fix

--- app/src/main/java/com/eltonkola/nisi/ui/apps/AllApps.kt
+++ app/src/main/java/com/eltonkola/nisi/ui/apps/AllApps.kt
@@ -20,12 +20,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.tv.material3.Text
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import com.eltonkola.nisi.R
 import com.eltonkola.nisi.data.model.AppSettingItem
 import com.eltonkola.nisi.ui.launcher.AppItemUi
 
@@ -63,7 +68,7 @@ fun AllApps(
             else -> {
                 val context = LocalContext.current
                 AppGrid(
-                    visibleApps = uiState.visibleApps,
+                    uiState = uiState,
                     openApp = { packageName -> viewModel.launchApp(context, packageName) }
                 )
             }
@@ -78,19 +83,44 @@ fun AllApps(
 
 @Composable
 fun AppGrid(
-    visibleApps: List<AppSettingItem>,
+    uiState: AppsUiState,
     openApp: (String) -> Unit
 ) {
 
     Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
 
+        val context = LocalContext.current
+
+        val wallpaperIdentifier = uiState.selectedWallpaperIdentifier
+        val isOffline = wallpaperIdentifier.startsWith("drawable/")
+        val imageModel = ImageRequest.Builder(context)
+            .data(
+                if (isOffline) {
+                    context.resources.getIdentifier(
+                        wallpaperIdentifier.substringAfter('/'),
+                        wallpaperIdentifier.substringBefore('/'),
+                        context.packageName
+                    ).takeIf { it != 0 } ?: R.drawable.offline_wallpaper_0
+                } else {
+                    wallpaperIdentifier
+                }
+            )
+            .crossfade(true)
+            .build()
+
+        AsyncImage(
+            model = imageModel,
+            contentDescription = "Background Wallpaper",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop // Crop to fill the entire screen
+        )
+
         Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.4f)
+                .fillMaxSize()
                 .background(
                     Brush.verticalGradient(
-                        colors = listOf(Color.Transparent, Color.Black.copy(alpha = 0.8f))
+                        colors = listOf(Color.White.copy(alpha = 0.2f), Color.Black.copy(alpha = 0.4f))
                     )
                 )
         )
@@ -105,7 +135,7 @@ fun AppGrid(
             verticalArrangement = Arrangement.spacedBy(24.dp), // Spacing between rows
             horizontalArrangement = Arrangement.spacedBy(20.dp) // Spacing between columns
         ) {
-            items (visibleApps, key = { it.packageName }) { app ->
+            items (uiState.visibleApps, key = { it.packageName }) { app ->
                 AppItemUi(
                     app = app,
                     onClick = {
